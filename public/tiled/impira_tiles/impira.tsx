f0846cc2ba395b9703a97d7f8b666e089e0396f6
Joren Lauwers <jorenlauwers@live.be>
2020-05-29 22:01:08 -0700

    Check in tiled folder

---
+++ public/tiled/impira_tiles/impira.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="impira" tilewidth="48" tileheight="73" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="16" source="floor1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="73" source="wall_n.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="73" source="wall_n_w.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="73" source="wall_w.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="73" source="door_w.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="73" source="door_w_open.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="41" source="table1_ew.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="41" source="table1_ns.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="33" source="table_end_e.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="33" source="table_end_w.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="33" source="table_mid_ew.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="33" source="computer.png"/>
+ </tile>
+</tileset>
