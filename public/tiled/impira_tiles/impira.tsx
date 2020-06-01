0d98bc43fb5089f6ec468412445a9d58e6920d09
Joren Lauwers <jorenlauwers@live.be>
2020-06-01 11:06:45 -0700

    Lots of new map resources

--- public/tiled/impira_tiles/impira.tsx
+++ public/tiled/impira_tiles/impira.tsx
@@ -1,6 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="impira" tilewidth="48" tileheight="73" tilecount="12" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
+<tileset version="1.2" tiledversion="1.3.4" name="impira" tilewidth="48" tileheight="73" tilecount="31" columns="0">
+ <editorsettings>
+  <export target="impira.json" format="json"/>
+ </editorsettings>
+ <grid orientation="isometric" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="16" source="floor1.png"/>
  </tile>
@@ -34,7 +37,64 @@
  <tile id="10">
   <image width="32" height="33" source="table_mid_ew.png"/>
  </tile>
- <tile id="11">
-  <image width="32" height="33" source="computer.png"/>
+ <tile id="12">
+  <image width="32" height="49" source="chair_e.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="49" source="chair_n.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="49" source="chair_s.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="49" source="chair_w.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="49" source="aeron_e.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="49" source="aeron_n.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="49" source="aeron_s.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="49" source="aeron_w.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="33" source="table_end_n.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="33" source="table_end_s.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="33" source="table_mid_ns.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="73" source="glass_wall_n.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="73" source="glass_wall_w.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="33" source="computer_s.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="33" source="computer_w.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="33" source="computer_e.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="33" source="computer_n.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="33" source="couch_green_e_s.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="33" source="couch_green_ew_s.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="33" source="couch_green_w_s.png"/>
  </tile>
 </tileset>
